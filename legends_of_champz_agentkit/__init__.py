39b301cacd427c5e8c74afbe7a4bb7dfc893843c
awstrade <awstradefree@gmail.com>
2026-06-28 19:30:19 +0200

    docs update

--- legends_of_champz_agentkit/__init__.py
+++ legends_of_champz_agentkit/__init__.py
@@ -3,7 +3,7 @@
 from .action_provider import LoCActionProvider, loc_action_provider
 from .registration import register_with_arena
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "LoCActionProvider",
